0531401ef999b7bc5839dbef31ec27acb1ab5e56
Vivian <vynride@gmail.com>
2025-06-27 00:36:04 +0530

    ui: add lander

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function Footer() {
+  return (
+    <div className="mb-3">
+      <h2 className="text-center text-gray-400 text-base rounded-4xl px-3 py-1">
+        <Link href="https://github.com/vynride" target="_blank">
+          Made with ❤️ by Vivian
+        </Link>
+      </h2>
+    </div>
+  );
+}
